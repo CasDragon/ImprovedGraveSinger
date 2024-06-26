ea8881cf6aafbd6a167dbcd7ab7451714659c5df
CasDragon <redyugi112@gmail.com>
2024-06-26 11:35:44 -0400

    Add project files.

---
+++ ImprovedGraveSinger/Main.cs
@@ -0,0 +1,73 @@
+﻿using BlueprintCore.Utils;
+using HarmonyLib;
+using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.UI.MVVM._VM.Crusade.Recruit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityModManagerNet;
+
+namespace ImprovedGraveSinger
+{
+#if DEBUG
+    [EnableReloading]
+#endif
+    public static class Main
+    {
+        internal static Harmony HarmonyInstance;
+        public static readonly LogWrapper logger = LogWrapper.Get("improvedgravesinger");
+
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+#if DEBUG
+            modEntry.OnUnload = OnUnload;
+#endif
+            modEntry.OnGUI = OnGUI;
+            HarmonyInstance = new Harmony(modEntry.Info.Id);
+            HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+            return true;
+        }
+
+        public static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+
+        }
+
+#if DEBUG
+        public static bool OnUnload(UnityModManager.ModEntry modEntry) {
+            HarmonyInstance.UnpatchAll(modEntry.Info.Id);
+            return true;
+        }
+#endif
+    }
+    [HarmonyPatch(typeof(BlueprintsCache))]
+    static class BlueprintsCaches_Patch
+    {
+        private static bool Initialized = false;
+
+        [HarmonyPriority(Priority.First)]
+        [HarmonyPatch(nameof(BlueprintsCache.Init)), HarmonyPostfix]
+        static void Postfix()
+        {
+            try
+            {
+                if (Initialized)
+                {
+                    Main.logger.Info("Already initialized blueprints cache.");
+                    return;
+                }
+                Initialized = true;
+
+                Main.logger.Info("Patching Grave Singer enchantment.");
+                Weapon.Configure();
+            }
+            catch (Exception e)
+            {
+                Main.logger.Error("Failed to initialize." + e);
+            }
+        }
+    }
+}
---
+++ ImprovedGraveSinger/Weapon.cs
@@ -0,0 +1,32 @@
+﻿using BlueprintCore.Blueprints.Configurators.Items;
+using BlueprintCore.Blueprints.Configurators.Items.Ecnchantments;
+using BlueprintCore.Blueprints.Configurators.Items.Weapons;
+using BlueprintCore.Blueprints.References;
+using Kingmaker.Blueprints;
+using Kingmaker.Designers.Mechanics;
+using Kingmaker.Designers.Mechanics.WeaponEnchants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImprovedGraveSinger
+{
+    internal class Weapon
+    {
+        public static void Configure()
+        {
+            ItemWeaponConfigurator.For(ItemWeaponRefs.RovagugRelicSteppeHandaxeItem.Reference.Get())
+                .AddToEnchantments(WeaponEnchantmentRefs.ViciousEnchantment.Reference.Get())
+                .Configure();
+            ItemWeaponConfigurator.For(ItemWeaponRefs.RovagugRelicSteppeGreataxeItem.Reference.Get())
+                .AddToEnchantments(WeaponEnchantmentRefs.ViciousEnchantment.Reference.Get())
+                .Configure();
+            ItemWeaponConfigurator.For(ItemWeaponRefs.RovagugRelicSteppeBattlaxeItem.Reference.Get())
+                .AddToEnchantments(WeaponEnchantmentRefs.ViciousEnchantment.Reference.Get())
+                .Configure();
+            WeaponEnchantmentRefs.RovagugRelicSteppeEnchantment.Reference.Get().GetComponent<WeaponCriticalEdgeStackable>().Bonus = 10;
+        }
+    }
+}
